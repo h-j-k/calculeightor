412c1431db3f80554d714a5d2b683a4a369a7ffa
h-j-k <han.junkwang@gmail.com>
2015-04-25 16:42:03 +0800

    added Javadoc comments.

--- src/main/java/com/ikueb/calculeightor/CalculeightorGUI.java
+++ src/main/java/com/ikueb/calculeightor/CalculeightorGUI.java
@@ -24,6 +24,19 @@ import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 import javax.swing.SwingConstants;
 
+/**
+ * The GUI implementation of {@link Calculeightor}.
+ * <p>
+ * The input sequence is enforced by enabling different buttons at each stage. The
+ * stages are:
+ * <ol>
+ * <li>Get a number</li>
+ * <li>Select an operator</li>
+ * <li>Get the next number</li>
+ * <li>Press the {@code "="} button to compute the answer</li>
+ * </ol>
+ * Results are displayed as integer values if possible.
+ */
 public class CalculeightorGUI extends JFrame implements Calculeightor<Double> {
 
     private static final long serialVersionUID = -1L;
@@ -34,8 +47,13 @@ public class CalculeightorGUI extends JFrame implements Calculeightor<Double> {
     private final JLabel label = new JLabel();
     private final List<Double> inputs = new ArrayList<>(2);
     private BinaryOperator<Double> operator;
-    private State state = null;
+    private State state;
 
+    /**
+     * Controls what {@link Component}s need to be configured at different stages of
+     * receiving input by calling {@link State#set(CalculeightorGUI)}. As such, the
+     * values represent what <em>has already</em> happened, and <em>has been</em> set.
+     */
     private enum State {
         INIT {
             @Override
@@ -80,7 +98,6 @@ public class CalculeightorGUI extends JFrame implements Calculeightor<Double> {
             Stream.of(components).flatMap(Function.identity())
                     .forEach(c -> c.setEnabled(!c.isEnabled()));
         }
-
     }
 
     public CalculeightorGUI() {
@@ -94,9 +111,11 @@ public class CalculeightorGUI extends JFrame implements Calculeightor<Double> {
         setVisible(true);
     }
 
+    /**
+     * Updates the latest state and also the input configuration.
+     */
     private void updateState() {
-        state = state == null ? State.INIT : state.next();
-        state.set(this);
+        (state = state == null ? State.INIT : state.next()).set(this);
     }
 
     private JPanel numbersPanel() {
@@ -117,11 +136,23 @@ public class CalculeightorGUI extends JFrame implements Calculeightor<Double> {
         });
     }
 
+    /**
+     * Creates {@link JButton}s representing each stream's elements.
+     *
+     * @param values
+     * @param collection the collection to add the created {@link JButton}s to
+     * @param listener the listener to receive events from the {@link JButton}s
+     * @return a single-row {@link JPanel} containing the created {@link JButton}s
+     */
     private <T> JPanel panelOf(Stream<T> values, Collection<JButton> collection,
             ActionListener listener) {
         return panelOf(values.map(v -> createButton(v, listener)).peek(collection::add));
     }
 
+    /**
+     * @param components
+     * @return a single-row {@link JPanel} containing the {@link Component}s
+     */
     private static <T extends Component> JPanel panelOf(Stream<T> components) {
         JPanel panel = new JPanel(new GridLayout());
         components.forEach(panel::add);
@@ -138,8 +169,14 @@ public class CalculeightorGUI extends JFrame implements Calculeightor<Double> {
         });
     }
 
-    private static JButton createButton(Object component, ActionListener listener) {
-        String text = component.toString();
+    /**
+     * @param input the input to use
+     * @param listener the listener to receive events from the {@link JButton}
+     * @return a {@link JButton} with the input as its text and a key binding of the
+     *         first character while the window is in focus
+     */
+    private static JButton createButton(Object input, ActionListener listener) {
+        String text = input.toString();
         JButton button = new JButton(text);
         button.addActionListener(listener);
         button.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
@@ -148,6 +185,10 @@ public class CalculeightorGUI extends JFrame implements Calculeightor<Double> {
         return button;
     }
 
+    /**
+     * @param button the {@link JButton} to configure for
+     * @return an {@link Action} that clicks the button
+     */
     @SuppressWarnings("serial")
     private static Action getAction(JButton button) {
         return new AbstractAction() {
