60e81890030602ab4c31eaf0aba5cf5c27926d84
h-j-k <han.junkwang@gmail.com>
2015-04-26 14:04:08 +0800

    refactored unit test.

--- src/test/java/com/ikueb/calculeightor/CalculeightorCommandLineInterfaceTest.java
+++ src/test/java/com/ikueb/calculeightor/CalculeightorCommandLineInterfaceTest.java
@@ -8,12 +8,11 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Scanner;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -25,12 +24,12 @@ public class CalculeightorCommandLineInterfaceTest {
     enum TestCase {
         EXIT,
         ONE_INTEGER("1"),
-        ONE_INTEGER_AND_OPERATOR("1", "+"),
+        ONE_INTEGER_AND_OPERATOR("0", "+"),
         ADD(Arrays.asList("3"), "1", "2", "+"),
         SUBTRACT(Arrays.asList("-1"), "3", "4", "-"),
         MULTIPLY(Arrays.asList("30"), "5", "6", "*"),
         EXPONENT(Arrays.asList("5764801"), "7", "8", "^"),
-        MULTI(Arrays.asList("19", "132"), "9", "10", "+", "11", "12", "*");
+        MULTI(Arrays.asList("19", "132"), "9", "10", "+", "-11", "-12", "*");
 
         private final List<String> expected;
         private final String[] inputs;
@@ -50,22 +49,23 @@ public class CalculeightorCommandLineInterfaceTest {
             assertThat(parseOutput(out), equalTo(expected));
         }
 
+        /**
+         * @param inputs the inputs to use
+         * @return an {@link InputStream} of the inputs
+         */
         private static InputStream toInputStream(String... inputs) {
-            String[] values = Arrays.copyOf(inputs, inputs.length + 1);
-            values[inputs.length] = CalculeightorCLI.END_INPUT;
-            return new ByteArrayInputStream(Arrays.stream(values)
+            return new ByteArrayInputStream(Arrays.stream(inputs)
                     .collect(Collectors.joining("\n")).getBytes(StandardCharsets.UTF_8));
         }
 
+        /**
+         * @param out the output to parse
+         * @return the {@link List} of calculation results
+         */
         private static List<String> parseOutput(OutputStream out) {
-            List<String> result = new ArrayList<>();
             int prefix = CalculeightorCLI.RESULT_PREFIX.length();
-            try (Scanner scanner = new Scanner(out.toString().trim())) {
-                while (scanner.hasNextLine()) {
-                    result.add(scanner.nextLine().substring(prefix));
-                }
-            }
-            return result;
+            return Pattern.compile("\\v+").splitAsStream(out.toString().trim())
+                    .map(i -> i.substring(prefix)).collect(Collectors.toList());
         }
     }
 
